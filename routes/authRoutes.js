eced4b6b6c7c30b0b9f8486ac0dfaf0bcfc62c49
Vinicius Dias <viniciusdias@alunos.utfpr.edu.br>
2025-06-30 22:27:24 -0300

    Arquivo ENV

---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/authController');
+
+router.get('/', controller.paginaInicial);
+router.post('/register', controller.registro);
+router.get('/login', controller.loginPage);
+router.post('/login', controller.login);
+router.get('/dashboard', controller.dashboard);
+
+module.exports = router;
